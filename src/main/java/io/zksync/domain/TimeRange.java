16714246567a64b6da5dd4de6692bb19a9396ac5
MaximFischuk <mfischuk@vareger.com>
2021-03-09 11:54:40 +0200

    Add default constructor for TimeRange with maximum validity range of transaction

--- src/main/java/io/zksync/domain/TimeRange.java
+++ src/main/java/io/zksync/domain/TimeRange.java
@@ -2,12 +2,15 @@ package io.zksync.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class TimeRange {
     private long validFrom;
     private long validUntil;
+
+    public TimeRange() {
+        this.validFrom = 0;
+        this.validUntil = 4294967295L;
+    }
 }
