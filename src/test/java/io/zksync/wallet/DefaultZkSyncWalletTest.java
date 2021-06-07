c88414a14b8c53a9dd68e061bc5763d013640dc9
MaximFischuk <mfischuk@vareger.com>
2021-06-07 15:47:07 +0300

    Fix tests to use new signatures

--- src/test/java/io/zksync/wallet/DefaultZkSyncWalletTest.java
+++ src/test/java/io/zksync/wallet/DefaultZkSyncWalletTest.java
@@ -220,7 +220,7 @@ public class DefaultZkSyncWalletTest {
             13,
             Signature.builder()
                 .pubKey("40771354dc314593e071eaf4d0f42ccb1fad6c7006c57464feeb7ab5872b7490")
-                .signature("31a6be992eeb311623eb466a49d54cb1e5b3d44e7ccc27d55f82969fe04824aa92107fefa6b0a2d7a07581ace7f6366a5904176fae4aadec24d75d3d76028500")
+                .signature("85782959384c1728192b0fe9466a4273b6d0e78e913eea894b780e0236fc4c9d673d3833e895bce992fc113a4d16bba47ef73fed9c4fca2af09ed06cd6885802")
                 .build(),
             new ChangePubKeyOnchain(),
             new TimeRange(0, 4294967295L)
@@ -237,7 +237,7 @@ public class DefaultZkSyncWalletTest {
             12,
             Signature.builder()
                 .pubKey("40771354dc314593e071eaf4d0f42ccb1fad6c7006c57464feeb7ab5872b7490")
-                .signature("50a9b498ffb54a24ba77fca2d9a72f4d906464d14c73c8f3b4a457e9149ba0885c6de37706ced49ae8401fb59000d4bcf9f37bcdaeab20a87476c3e08088b702")
+                .signature("b1b82f7ac37e2d4bd675e4a5cd5e48d9fad1739282db8a979c3e4d9e39d794915667ee2c125ba24f4fe81ad6d19491eef0be849a823ea6567517b7e207214705")
                 .build(),
             new TimeRange(0, 4294967295L)
         );
@@ -255,7 +255,7 @@ public class DefaultZkSyncWalletTest {
             12,
             Signature.builder()
                 .pubKey("40771354dc314593e071eaf4d0f42ccb1fad6c7006c57464feeb7ab5872b7490")
-                .signature("5c3304c8d1a8917580c9a3f8edb9d8698cbe9e6e084af93c13ac3564fa052588b93830785b3d0f60a1a193ec4fff61f81b95f0d16bf128ee21a6ceb09ef88602")
+                .signature("b3211c7e15d31d64619e0c7f65fce8c6e45637b5cfc8711478c5a151e6568d875ec7f48e040225fe3cc7f1e7294625cad6d98b4595d007d36ef62122de16ae01")
                 .build(),
             null,
             new TimeRange(0, 4294967295L)
@@ -274,7 +274,7 @@ public class DefaultZkSyncWalletTest {
             12,
             Signature.builder()
                 .pubKey("40771354dc314593e071eaf4d0f42ccb1fad6c7006c57464feeb7ab5872b7490")
-                .signature("3e2866bb00f892170cc3592d48aec7eb4afba75bdd0a530780fa1dcbdf857d07d75deb774142a93e3d1ca3be29e614e50892b95702b6461f86ddf78b9ab11a01")
+                .signature("11dc47fced9e6ffabe33112a4280c02d0c1ffa649ba3843eec256d427b90ed82e495c0cee2138d5a9e20328d31cb97b70d7e2ede0d8d967678803f4b5896f701")
                 .build(),
             new TimeRange(0, 4294967295L)
         );
@@ -292,7 +292,7 @@ public class DefaultZkSyncWalletTest {
             12,
             Signature.builder()
                 .pubKey("40771354dc314593e071eaf4d0f42ccb1fad6c7006c57464feeb7ab5872b7490")
-                .signature("8c119b01ff8ae75ba5aabaa4ad480690e6a56d6e99d430ecac3bc3beacbaba28b3740cb20574d130281874fc70daaab884ee8e03a510e9ca9c1c677a2412cf03")
+                .signature("5cf4ef4680d58e23ede08cc2f8dd33123c339788721e307a813cdf82bc0bac1c10bc861c68d0b5328e4cb87b610e4dfdc13ddf8a444a4a2ac374ac3c73dbec05")
                 .build()
         );
         return tx;
@@ -309,7 +309,7 @@ public class DefaultZkSyncWalletTest {
             12,
             Signature.builder()
                 .pubKey("40771354dc314593e071eaf4d0f42ccb1fad6c7006c57464feeb7ab5872b7490")
-                .signature("9d94324425f23d09bf76df52e520e8da4561718057eb29fe6d760945be986b8e3a1955d9c02cf415558f533b7d9573564798db9586cc5ba1fdc44f711e455e03")
+                .signature("1236180fe01b42c0c3c084d152b0582e714fa19da85900777e811f484a5b3ea434af320f66c7c657a33024d7be22cea44b7406d0af88c097a9d7d6b5d7154d02")
                 .build(),
             new TimeRange(0, 4294967295L)
         );
